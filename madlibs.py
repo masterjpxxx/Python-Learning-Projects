1e90a280197d6ab886786a1a2dc9f38ac30cdab7
masterjpxxx <johnpearl.manungas@pantheon.io>
2023-03-05 00:20:09 +0800

    Done: madlib

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# This project revolves around usibg madlib
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famousperson = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famousperson}!"
+
+print(madlib)

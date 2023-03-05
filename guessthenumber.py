df9375ed290890b1cd241521bb308a3a0d57719d
masterjpxxx <johnpearl.manungas@pantheon.io>
2023-03-05 15:40:31 +0800

    Done: rock paper scissors

--- guessthenumber.py
+++ guessthenumber.py
@@ -14,5 +14,25 @@ def guess(x):
             print("Sorry guess again. Then number is too high!")
         
     print(f"Congratulation you guessed it right! The number is {random_number}!")
+ 
+#guess(10)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+  
+    while feedback != 'c' and low != high:
+        guess = random.randint(low,high)
+        feedback = input(f"Is {guess} the number correct? L - for lower, H - for Higher and C - Correct: ".lower())
+        if feedback == 'l':
+            high = guess -1 
+        elif feedback == 'h':
+            low = guess + 1
+            
+    print(f"Congratulations! you have guessed the number which is {guess}!")
+
+computer_guess(6)        
     
-guess(10)
+
+    

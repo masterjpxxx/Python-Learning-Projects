ad9c43710d8b40268a1a5a97985524104aea312e
masterjpxxx <johnpearl.manungas@pantheon.io>
2023-03-06 14:55:23 +0800

    Updated Hangman

--- hangman.py
+++ hangman.py
@@ -11,13 +11,16 @@ def play():
     wordletters = set(word)
     alphabet = set(string.ascii_lowercase)
     used_letters = set()
-    print(wordletters)
-    while len(wordletters) > 0:
+    life = 8
+    #print(wordletters)
+    while life > 0 and len(wordletters) > 0:
         user_letter = input("Guess a Letter: ")
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in wordletters:
                 wordletters.remove(user_letter)
+            else:
+                life = life - 1
         elif user_letter in used_letters:
             print("You have already used that letter!")
         else:
@@ -25,10 +28,13 @@ def play():
             
         print("You have used the letters: ", " ".join(used_letters))
         wordlist = [letter if letter in used_letters else '-' for letter in word]
-        print("Current word is: ", " ".join(wordlist))
+        print(f"Remaining Lives: {life} Current word is: ", " ".join(wordlist))
     
+    #print(len(wordletters))
     if len(wordletters) == 0 :
         print("Congratulations! You have guessed the correct word!")
+    if life == 0:
+        print("Sorry you have failed to guessed the word!")
 
 play()
     

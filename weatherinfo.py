fc29d1f711f645482732dde4b0bd5e4237b9cd65
masterjpxxx <johnpearl.manungas@pantheon.io>
2023-03-12 23:53:32 +0800

    Done Weather info generator

---
+++ weatherinfo.py
@@ -0,0 +1,20 @@
+#This python program is used to generate weather data from a specified location by the user
+#Step1: Import pprint and requests library'
+#Step2: Register and generate an API key for weather generator website
+#Step3: Ask the user for the location
+#Step4: Generate the weather data using the API Call
+#Step5: Display the result to the user
+
+import requests
+from pprint import pprint
+
+
+def generateweatherdata(location):
+    API_KEY = '81acc6acb02af547474ca2beac56f67f'
+    base_url ='http://api.openweathermap.org/data/2.5/weather?appid='+API_KEY+"&q="+location
+    weatherdata = requests.get(base_url).json()
+    pprint(weatherdata)
+    
+    
+location = input("Please specify a location: ")
+generateweatherdata(location)
